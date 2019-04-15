2a97453f69ecbfc4f8202596ca02e7e863c90aa6
karawebb <kwebb.creation@gmail.com>
2019-04-15 16:43:51 -0700

    Added Signup info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
 protect_from_forgery prepend:true
+ before_action :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_in_path(resource)
         protected_path
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :hours])
     end
 end
