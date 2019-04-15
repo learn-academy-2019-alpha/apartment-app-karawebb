2b44ee82b7770a14c27802ff4137dfec6f791ba1
karawebb <kwebb.creation@gmail.com>
2019-04-15 13:43:11 -0700

    new migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :apartments
   devise_for :users
   get 'protected', to: 'pages#protected'
   get 'unprotected', to: 'pages#unprotected'
 
-  root to: "home#index"
+  root to: "pages#unprotected"
 end
--- db/migrate/20190415182824_devise_create_users.rb
+++ db/migrate/20190415182824_devise_create_users.rb
@@ -7,6 +7,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## Contact info
+     t.string :name
+     t.string :phone_number
+     t.string :hours
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
