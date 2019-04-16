0b32807ef01eda8318285bc2b2df8080c4389336
karawebb <kwebb.creation@gmail.com>
2019-04-15 17:05:31 -0700

    working sign up and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :apartments
   devise_for :users
-  
+
   get "*path", to: 'pages#protected', constraints: ->(request){ request.format.html? }
-  get "protected", to: 'pages#protected', as: :protected
+  get "/protected", to: 'pages#protected', as: :protected
 
   root to: "pages#unprotected"
 end
